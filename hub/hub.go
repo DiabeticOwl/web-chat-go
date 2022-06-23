12282ff58246fdf0b6bafa3c01a8b9247a4e0776
DiabeticOwl <jh.cruz91@gmail.com>
2022-06-23 18:18:15 -0400

    Cleansing of the mutex and waitgroups.

--- hub/hub.go
+++ hub/hub.go
@@ -8,9 +8,6 @@ import (
 	"sync"
 )
 
-var wg = &sync.WaitGroup{}
-var mut = &sync.RWMutex{}
-
 type Hub struct {
 	// Map of WebSocket clients that will be used to broadcast messages to the
 	// rest of its own type.
@@ -50,56 +47,36 @@ func (h *Hub) closeClient(client *Client) {
 	close(client.Send)
 }
 
-// waitAndRun will wrap a function with an n amount of waitgroups and lock the
-// resources used in that function with a RWMutex before realising them and
-// telling the WaitGroup instance that all groups have completed their tasks.
-func (h *Hub) waitAndRun(f func(), n int) {
-	wg.Add(n)
-
-	mut.Lock()
-
-	f()
-
-	mut.Unlock()
-
-	wg.Done()
-}
-
 func (h *Hub) Run() {
 	for {
 		select {
 		// Each Client's registration.
 		case client := <-h.registerWS:
-			h.waitAndRun(func() {
-				h.clientsWS[client] = true
-			}, 1)
+			h.clientsWS[client] = true
 		// Each ClientTCP's registration.
 		case client := <-h.RegisterTCP:
-			h.waitAndRun(func() {
-				h.clientsTCP[client] = true
-			}, 1)
+			h.clientsTCP[client] = true
 		// Each Client's unregistration.
 		case client := <-h.unregisterWS:
-			h.waitAndRun(func() {
-				if _, ok := h.clientsWS[client]; ok {
-					h.closeClient(client)
-				}
-			}, 1)
+			if _, ok := h.clientsWS[client]; ok {
+				h.closeClient(client)
+			}
 		// Each Client's unregistration.
 		case client := <-h.UnregisterTCP:
-			h.waitAndRun(func() {
-				if _, ok := h.clientsTCP[client]; ok {
-					delete(h.clientsTCP, client)
-				}
-			}, 1)
+			if _, ok := h.clientsTCP[client]; ok {
+				delete(h.clientsTCP, client)
+			}
 		// Each Client's message to be broadcasted.
 		// Each message is passed to two goroutines that will range hub's
 		// maps of clients.
 		case msg := <-h.Broadcast:
+			var wg = &sync.WaitGroup{}
+
 			wg.Add(2)
 
 			go func() {
-				mut.RLock()
+				defer wg.Done()
+
 				for client := range h.clientsWS {
 					select {
 					// Sends the message extracted from the broadcast channel
@@ -109,26 +86,25 @@ func (h *Hub) Run() {
 						h.closeClient(client)
 					}
 				}
-				mut.RUnlock()
-
-				wg.Done()
 			}()
 
-			for client := range h.clientsTCP {
-				msgDet := strings.Split(string(msg), "|")
+			go func() {
+				defer wg.Done()
 
-				tm, ms, un := msgDet[0], msgDet[1], msgDet[2]
+				for client := range h.clientsTCP {
+					msgDet := strings.Split(string(msg), "|")
 
-				if client.User.UserName == un {
-					continue
+					tm, ms, un := msgDet[0], msgDet[1], msgDet[2]
+
+					if client.User.UserName == un {
+						continue
+					}
+
+					msg := fmt.Sprintf("%v - User %v says: %v", tm, un, ms)
+
+					fmt.Fprintln(client.Conn, msg)
 				}
-
-				msg := fmt.Sprintf("%v - User %v says: %v", tm, un, ms)
-
-				fmt.Fprintln(client.Conn, msg)
-			}
-
-			wg.Done()
+			}()
 		}
 	}
 }
